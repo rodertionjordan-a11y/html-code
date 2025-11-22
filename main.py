1043ab76dcb7036387f33a9302bd2f1efe71e2a4
rodertionjordan-a11y <rodertionjordan@gmail.com>
2025-11-22 15:57:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Импорт
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -15,7 +15,6 @@ def result_calculate(size, lights, device):
 @app.route('/')
 def index():
     return render_template('index.html')
-
 #Вторая страница
 @app.route('/<size>')
 def lights(size):
@@ -28,7 +27,7 @@ def lights(size):
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
-                            'electronics.html',
+                            'electronics.html',                           
                             size = size, 
                             lights = lights                           
                            )
@@ -42,4 +41,32 @@ def end(size, lights, device):
                                                     int(device)
                                                     )
                         )
+#Форма
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Результаты формы
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    #Создай переменные для сбора информации
+    name = request.form['name']
+    email = request.form['email']
+    address = request.form['address']
+    date = request.form['date']
+    with open("file.txt", mode="a") as f:
+        f.write("Имя: " + name + "\n")
+        f.write("Почта: " + email + "\n")
+        f.write("Адрес: " + address + "\n")
+        f.write("Дата: " + date + "\n\n")
+
+    # здесь вы можете сохранить данные или отправить их по электронной почте
+    return render_template('form_result.html', 
+                           #Помести переменные
+                           name=name,
+                           email=email,
+                           address=address,
+                           date=date,
+                           )
+
 app.run(debug=True)
